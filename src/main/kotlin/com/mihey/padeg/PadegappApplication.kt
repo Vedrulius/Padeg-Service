e96c734f03297f6f5c6e44d94f15825c1f4d0c89
Mikhail Dolgov <otetc82@yandex.ru>
2021-03-28 20:20:44 +0300

    initial commit

---
+++ src/main/kotlin/com/mihey/padeg/PadegappApplication.kt
@@ -0,0 +1,11 @@
+package com.mihey.padeg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PadegappApplication
+
+fun main(args: Array<String>) {
+    runApplication<PadegappApplication>(*args)
+}
---
+++ src/test/kotlin/com/mihey/padeg/PadegappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mihey.padeg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PadegappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
